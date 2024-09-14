b5dd9f907bf24edcb9f979ff6068690af77b9983
Lascu Mirela <lascu.mirela14@gmail.com>
2024-09-14 20:53:57 +0300

    Colors Entity (Admin)

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -36,6 +36,12 @@ export function MainNav ({
             label: 'Sizes',
             active: pathname === `/${params.storeId}/sizes`,
         },
+
+        {
+            href: `/${params.storeId}/colors`,
+            label: 'Colors',
+            active: pathname === `/${params.storeId}/colors`,
+        },
         
         {
             href: `/${params.storeId}/settings`,
